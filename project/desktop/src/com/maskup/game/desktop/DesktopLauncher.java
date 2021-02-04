cc573e2bd819fece1b908ef7049d417ef5c4bcd5
Phong Bach <phongnghinhbach@gmail.com>
2021-02-04 01:27:14 -0800

    Add a blank MaskUp project

---
+++ project/desktop/src/com/maskup/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.maskup.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.maskup.game.MaskUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MaskUp(), config);
+	}
+}
