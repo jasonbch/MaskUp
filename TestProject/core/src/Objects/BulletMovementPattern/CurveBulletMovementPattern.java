71a9bdf1d1b19199fc45dc5cc72440458eb00b2f
PHONG BACH <phongnghinhbach@gmail.com>
2021-04-30 16:10:21 -0700

    Finish

--- TestProject/core/src/Objects/BulletMovementPattern/CurveBulletMovementPattern.java
+++ TestProject/core/src/Objects/BulletMovementPattern/CurveBulletMovementPattern.java
@@ -12,12 +12,29 @@ public class CurveBulletMovementPattern extends BulletMovementPattern {
     @Override
     public void move(GameObject obj, float deltaTime) {
         Ammo ammo = (Ammo) obj;
-        ammo.setScaling(ammo.getScaling() + 10);
-        Ammo.PatternAttribute bulletPattern = ammo.getPatternAttribute();
-        float newX = ammo.getXPosition() + bulletPattern.getXMultiplier() * ammo.getSpeed() * deltaTime + ammo.getScaling() * deltaTime;
-        float newY = ammo.getYPosition() + bulletPattern.getYMultiplier() * ammo.getSpeed() * deltaTime - ammo.getScaling() * deltaTime;
 
-        ammo.setXPosition(newX);
-        ammo.setYPosition(newY);
+        float centerX = ammo.getOriginalX();
+        float centerY = ammo.getOriginalY();
+
+        // Initial radius
+        float radius = 50f;
+        float speed = 700f;
+        float rate = deltaTime;
+
+        // Scaling to expand circle
+        float scaling = ammo.getScaling() + deltaTime * 3f;
+
+        float circleX = (float) (Math.cos(ammo.getAngle()) * (radius * scaling) + centerX);
+        float circleY = (float) (Math.sin(ammo.getAngle()) * (radius * scaling) + centerY);
+        float angle = ammo.getAngle() + (speed * (rate / 1000)) % 360;
+
+        if (angle >= 360) {
+            angle = 0;
+        }
+
+        ammo.setAngle(angle);
+        ammo.setScaling(scaling);
+        ammo.setXPosition(circleX);
+        ammo.setYPosition(circleY + (ammo.getImageHeight() / 2));
     }
 }
