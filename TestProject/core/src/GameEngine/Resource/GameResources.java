bb1beeb8ce081069f3405ac3883460f3cd61b167
Russell Heppell <russell.heppell@wsu.edu>
2021-05-03 15:57:33 -0700

    starting to make JSON for final game. issue with covid and pattern 4.

--- TestProject/core/src/GameEngine/Resource/GameResources.java
+++ TestProject/core/src/GameEngine/Resource/GameResources.java
@@ -12,7 +12,7 @@ public class GameResources {
 
     // Implement Singleton
     private static GameResources uniqueInstance = null;
-    private static String gameJSON = "game.json";
+    private static String gameJSON = "game2.json";
     private final int WORLD_HEIGHT = Gdx.graphics.getHeight();
     private final int WORLD_WIDTH = Gdx.graphics.getWidth();
     private SpriteBatch batch = null;
