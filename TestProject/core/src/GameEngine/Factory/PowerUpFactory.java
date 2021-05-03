e21864af29af505b07d32240c1ab0b0fa12ea433
dnuon <danielnuon@Daniels-MacBook-Pro-16.local>
2021-05-03 15:54:58 -0700

    renamed stimulus pack to powerup for consistency

--- TestProject/core/src/GameEngine/Factory/PowerUpFactory.java
+++ TestProject/core/src/GameEngine/Factory/PowerUpFactory.java
@@ -5,7 +5,7 @@ import Objects.GameObject.PowerUp;
 import com.badlogic.gdx.graphics.Texture;
 
 public class PowerUpFactory {
-    private final Texture texture = GameResources.getAssetsManager().get("StimulusPack.png", Texture.class);
+    private final Texture texture = GameResources.getAssetsManager().get("PowerUp.png", Texture.class);
 
     public PowerUp create(float xPos, float yPos) {
         return new PowerUp(xPos - (texture.getWidth() / 2), yPos - texture.getHeight());
